710c81f2de9ff7017555248303ed1f5f0559f8e2
Baymax104 <wzy1048168235@163.com>
2024-07-07 02:14:37 +0800

    build: 测试

---
+++ _site/assets/js/data/swconf.js
@@ -0,0 +1,32 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1720289246',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+        '/about/',
+      
+
+      
+      
+        '/assets/js/dist/categories.min.js',
+        '/assets/js/dist/commons.min.js',
+        '/assets/js/dist/home.min.js',
+        '/assets/js/dist/misc.min.js',
+        '/assets/js/dist/page.min.js',
+        '/assets/js/dist/post.min.js'
+    ],allowHosts: [
+      
+    ],denyPaths: [
+      
+    ],
+    purge: false
+  
+};
+
