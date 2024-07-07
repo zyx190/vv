1fbef48501bb2089da9312d562cbb6ca0ba9ebb6
Baymax104 <wzy1048168235@163.com>
2024-07-07 18:13:39 +0800

    fix: 图片链接错误

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1720346719',resources: [
+    cacheName: 'chirpy-1720347182',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
